a33136593a22f85394f7b9a08b16eedcd1ce8bd8
Alexandru Gyori <alexandru.gyori@studentpartner.com>
2012-10-24 14:51:01 +0300

    Added the Environment class.

--- src/ro/upt/pcbe/workflowSimulator/Environment.java
+++ src/ro/upt/pcbe/workflowSimulator/Environment.java
@@ -1,19 +1,41 @@
 package ro.upt.pcbe.workflowSimulator;
 
+
+import java.util.AbstractMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 
 public class Environment {
-	
-	private ConcurrentHashMap<String,String> variablesMap;
-	
-   public String getVariable(String variableName) {
-	   return variablesMap.get(variableName);
 
-   }
-   
-   public void setVariable(String variableName, String variableValue) {
-	  
-   }
-   
-   }
+	private AbstractMap<String,String> variablesMap;
+
+	private static volatile Environment env;
+
+	private Environment(){
+		variablesMap=new ConcurrentHashMap<String, String>();
+	}
+
+	public static Environment getInstance()
+	{
+		if(env == null)
+		{
+			synchronized(Environment.class)
+			{
+				if(env == null)
+					env=new Environment();
+			}
+		}
+		return env;
+	}
+
+
+	public String getVariable(String variableName) {
+		return variablesMap.get(variableName);
+
+	}
+
+	public void setVariable(String variableName, String variableValue) {
+		variablesMap.put(variableName, variableValue);
+	}
+
+}
