b93c09818774c294349784e51bf915110e95338c
Alexandru Gyori <alexandru.gyori@studentpartner.com>
2012-10-24 14:48:14 +0300

    Condition class with immutable functionality

---
+++ src/ro/upt/pcbe/workflowSimulator/Condition.java
@@ -0,0 +1,24 @@
+package ro.upt.pcbe.workflowSimulator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Immutable
+public class Condition {
+
+	private Map<String,String> conditions;
+	public Condition(Map<String,String> map){
+		this.conditions=new HashMap<String, String>(map);
+	}
+	
+	public boolean evaluate(Environment env)
+	{
+		for(String str : conditions.keySet())
+		{
+			if(!conditions.get(str).equals(env.getVariable(str)))
+				return false;
+		}
+		
+		return true;
+	}
+}
